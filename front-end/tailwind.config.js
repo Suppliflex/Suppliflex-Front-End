d53fc68e431b5dd303c4f6d97d92d9640cf51b1b
MoHammed AlFakir <its.mohammedalfakir@gmail.com>
2025-01-30 18:47:34 +0100

    Colors

--- front-end/tailwind.config.js
+++ front-end/tailwind.config.js
@@ -9,16 +9,6 @@ module.exports = {
         primary: '#272125',
         secondary: '#7F6C79',
         tertiary: '#F4F7FF',
-        // Customize Colors for dark mode
-        dark: {
-          background: 'rgb(0, 0, 0)',
-          text: 'rgb(244, 247, 255)',
-        },
-        // Customize Colors for light mode
-        light: {
-          background: 'rgb(252, 252, 252)',
-          text: 'rgb(26, 26, 26)',
-        },
       },
     },
   },
