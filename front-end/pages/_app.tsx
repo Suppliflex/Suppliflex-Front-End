cf4da51258787eeadfaa122dd25d5e2ea09590c6
MoHammed AlFakir <its.mohammedalfakir@gmail.com>
2025-01-30 18:31:06 +0100

    Setting dark mode as the default theme

--- front-end/pages/_app.tsx
+++ front-end/pages/_app.tsx
@@ -1,19 +1,11 @@
 import '../styles/globals.css';
 import type { AppProps } from 'next/app';
-import { useEffect } from 'react';
 
 function MyApp({ Component, pageProps }: AppProps) {
-  useEffect(() => {
-    // Check localStorage for saved theme
-    const savedTheme = localStorage.getItem('theme');
-    const htmlClass = document.documentElement.classList;
-
-    if (savedTheme === 'light') {
-      htmlClass.remove('dark');
-    } else {
-      htmlClass.add('dark'); // Default to dark mode if no preference
-    }
-  }, []);
+  // Force dark mode by default (add 'dark' class to <html>)
+  if (typeof window !== 'undefined') {
+    document.documentElement.classList.add('dark');
+  }
 
   return <Component {...pageProps} />;
 }
