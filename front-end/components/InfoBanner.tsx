026e95c740607edac6a65619859694cc0c11a856
victoria <itsfoxiefo@gmail.com>
2025-01-31 14:39:18 +0100

    added padding to InfoBanner

--- front-end/components/InfoBanner.tsx
+++ front-end/components/InfoBanner.tsx
@@ -2,7 +2,7 @@ const InfoBanner: React.FC = () => {
   return (
     <div className="bg-pink h-24 mt-20 flex items-center">
       <div className="max-w-7xl m-auto w-full">
-        <ul className="flex w-full justify-between">
+        <ul className="flex w-full justify-between pl-5 pr-5">
           <li className="flex items-center gap-6 text-3xl">
             {' '}
             <img src="./img/pictures/sparkle.svg" alt="sparkle" />
