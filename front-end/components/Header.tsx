947aeddc4b892583eab3ec7d353ca38f862c4285
victoria <itsfoxiefo@gmail.com>
2025-01-31 14:27:09 +0100

    styled Intro component

--- front-end/components/Header.tsx
+++ front-end/components/Header.tsx
@@ -4,7 +4,9 @@ const Header: React.FC = () => {
   return (
     <header className="max-w-7xl mx-auto flex justify-between pt-9 pb-9 pl-20 pr-20">
       <div className="flex gap-28">
-        <img src="img/logo/logo.svg" alt="logo" />
+        <a href="/">
+          <img src="img/logo/logo.svg" alt="logo" />
+        </a>
         <ul className="flex gap-8 items-center">
           <li>
             <a className=" font-semibold text-white hover:text-pink transition-colors" href="/">
--- front-end/components/InfoBanner.tsx
+++ front-end/components/InfoBanner.tsx
@@ -1,5 +1,37 @@
 const InfoBanner: React.FC = () => {
-  return <div className="max-w-7xl m-auto flex mt-24 justify-between items-center pl-5 pr-5">banner here</div>;
+  return (
+    <div className="bg-pink h-24 mt-20 flex items-center">
+      <div className="max-w-7xl m-auto w-full">
+        <ul className="flex w-full justify-between">
+          <li className="flex items-center gap-6 text-3xl">
+            {' '}
+            <img src="./img/pictures/sparkle.svg" alt="sparkle" />
+            LOGISTIC
+          </li>
+          <li className="flex items-center gap-6 text-3xl">
+            {' '}
+            <img src="./img/pictures/sparkle.svg" alt="sparkle" />
+            TRACKING
+          </li>
+          <li className="flex items-center gap-6 text-3xl">
+            {' '}
+            <img src="./img/pictures/sparkle.svg" alt="sparkle" />
+            DELIVERY
+          </li>
+          <li className="flex items-center gap-6 text-3xl">
+            {' '}
+            <img src="./img/pictures/sparkle.svg" alt="sparkle" />
+            FINANCE
+          </li>
+          <li className="flex items-center gap-6 text-3xl">
+            {' '}
+            <img src="./img/pictures/sparkle.svg" alt="sparkle" />
+            SHIPPING
+          </li>
+        </ul>
+      </div>
+    </div>
+  );
 };
 
 export default InfoBanner;
