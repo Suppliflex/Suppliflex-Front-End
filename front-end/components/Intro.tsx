93e0403569f35f59aaf36e0668d44a2ee920e20c
victoria <itsfoxiefo@gmail.com>
2025-01-31 11:55:18 +0100

    created Intro component

---
+++ front-end/components/Intro.tsx
@@ -0,0 +1,21 @@
+const Intro: React.FC = () => {
+  return (
+    <div className="max-w-7xl m-auto">
+      <div>
+        <h1 >Real-Time Supply Chain Management Software</h1>
+        <p>
+          Suppliflex provides an innovative platform with the overall goal to digitize and
+          streamline your supply chain management operations. Our real-time shipment tracking,
+          warehouse management, and automated billing solutions empower businesses to reduce costs
+          and improve efficiency.
+        </p>
+      </div>
+      <div>
+        <img src="./img/pictures/globe.svg" alt="globe" />
+        <img src="./img/pictures/tire-marks.svg" alt="" />
+      </div>
+    </div>
+  );
+};
+
+export default Intro;
--- front-end/pages/index.tsx
+++ front-end/pages/index.tsx
@@ -1,12 +1,14 @@
 import '../styles/globals.css';
 import ThemeToggle from '../components/ThemeToggle';
 import Header from './../components/Header';
+import Intro from './../components/Intro'
 
 export default function Home() {
   return (
-    <main className="bg-black mx-auto">
+    <main className="bg-black mx-auto text-white">
       <div className="min-h-screen transition-colors duration-300">
         <Header />
+        <Intro />
         {/* <ThemeToggle /> We can hide Toggle from here, until light mode is needed */}
       </div>
     </main>
