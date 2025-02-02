38d88301d9373552fd1d07322e09196a692adcb3
Victoria <itsfoxiefo@gmail.com>
2025-02-02 15:26:34 +0100

    updated font size in InfoBanner for responsive scaling

--- front-end/components/InfoBanner.tsx
+++ front-end/components/InfoBanner.tsx
@@ -1,33 +1,22 @@
 const InfoBanner: React.FC = () => {
+  const items = ["LOGISTIC", "TRACKING", "DELIVERY", "FINANCE", "SHIPPING"];
+
   return (
     <div className="bg-pink h-24 mt-20 flex items-center">
       <div className="max-w-7xl m-auto w-full">
-        <ul className="flex w-full justify-between pl-5 pr-5">
-          <li className="flex items-center gap-6 text-3xl">
-            {' '}
-            <img src="./img/pictures/sparkle.svg" alt="sparkle" />
-            LOGISTIC
-          </li>
-          <li className="flex items-center gap-6 text-3xl">
-            {' '}
-            <img src="./img/pictures/sparkle.svg" alt="sparkle" />
-            TRACKING
-          </li>
-          <li className="flex items-center gap-6 text-3xl">
-            {' '}
-            <img src="./img/pictures/sparkle.svg" alt="sparkle" />
-            DELIVERY
-          </li>
-          <li className="flex items-center gap-6 text-3xl">
-            {' '}
-            <img src="./img/pictures/sparkle.svg" alt="sparkle" />
-            FINANCE
-          </li>
-          <li className="flex items-center gap-6 text-3xl">
-            {' '}
-            <img src="./img/pictures/sparkle.svg" alt="sparkle" />
-            SHIPPING
-          </li>
+        <ul className="flex w-full justify-between px-5">
+          {items.map((item) => (
+            <li
+              key={item}
+              className="flex items-center gap-6"
+              style={{
+                fontSize: "clamp(0.5rem, -0.155rem + 2.33vw, 2rem)"
+              }}
+            >
+              <img src="./img/pictures/sparkle.svg" alt="sparkle" />
+              {item}
+            </li>
+          ))}
         </ul>
       </div>
     </div>
